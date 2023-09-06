2c3f08a83c73948843eb251c1a447d4f34f385f8
小比尔 <171882044@qq.com>
2023-09-06 14:34:18 +0800

    commit

---
+++ CSV Split/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace CSV_Split
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.txtRowCount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDoing = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.BackColor = System.Drawing.SystemColors.Window;
+            this.txtFilePath.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.txtFilePath.HideSelection = false;
+            this.txtFilePath.Location = new System.Drawing.Point(79, 33);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.ReadOnly = true;
+            this.txtFilePath.Size = new System.Drawing.Size(228, 21);
+            this.txtFilePath.TabIndex = 1;
+            this.txtFilePath.Text = "双击选择文件...";
+            this.txtFilePath.Enter += new System.EventHandler(this.txtFilePath_Enter);
+            this.txtFilePath.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.txtFilePath_MouseDoubleClick);
+            // 
+            // txtRowCount
+            // 
+            this.txtRowCount.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtRowCount.Location = new System.Drawing.Point(79, 66);
+            this.txtRowCount.Name = "txtRowCount";
+            this.txtRowCount.Size = new System.Drawing.Size(228, 21);
+            this.txtRowCount.TabIndex = 1;
+            this.txtRowCount.Text = "1,000,000";
+            this.txtRowCount.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "文件路径";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "折分行数";
+            // 
+            // btnDoing
+            // 
+            this.btnDoing.Location = new System.Drawing.Point(79, 106);
+            this.btnDoing.Name = "btnDoing";
+            this.btnDoing.Size = new System.Drawing.Size(75, 23);
+            this.btnDoing.TabIndex = 0;
+            this.btnDoing.Text = "Split";
+            this.btnDoing.UseVisualStyleBackColor = true;
+            this.btnDoing.Click += new System.EventHandler(this.btnDoing_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 141);
+            this.Controls.Add(this.btnDoing);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRowCount);
+            this.Controls.Add(this.txtFilePath);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "CSV Split";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.TextBox txtRowCount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDoing;
+    }
+}
+
---
+++ CSV Split/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CSV_Split
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Focus(); ActiveControl = null;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (double.TryParse(this.txtRowCount.Text.Replace(",", ""), out double number))
+            {
+                this.txtRowCount.Text = string.Format("{0:N0}", number); // N0 表示按千位分隔显示，没有小数部分
+                this.txtRowCount.SelectionStart = this.txtRowCount.Text.Length; // 将光标移到末尾
+            }
+        }
+
+        private void txtFilePath_Enter(object sender, EventArgs e)
+        {
+            // 清除全选状态，让文本框不会自动全选文本
+            this.txtFilePath.SelectionStart = this.txtFilePath.Text.Length;
+            this.txtFilePath.SelectionLength = 0;
+        }
+
+        private void txtFilePath_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.openFileDialog1.Filter = "CSV Files (*.csv)|*.csv";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.txtFilePath.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void btnDoing_Click(object sender, EventArgs e)
+        {
+            string inputFilePath = this.txtFilePath.Text;
+            int linesPerFile = int.Parse(this.txtRowCount.Text.Replace(",", ""));
+            this.SplitCSVFile(inputFilePath, linesPerFile);
+            MessageBox.Show("CSV splitting complete.");
+        }
+        private void SplitCSVFile(string inputFilePath, int linesPerFile)
+        {
+            FileInfo file = new FileInfo(inputFilePath);
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                int fileNumber = 1;
+                int currentLine = 0;
+
+                string header = reader.ReadLine(); // 读取列名
+                while (!reader.EndOfStream)
+                {
+                    string outputFileName = $"{file.Name}_{fileNumber}.csv";
+                    string outputFilePath = Path.Combine(file.DirectoryName, outputFileName);
+                    using (StreamWriter writer = new StreamWriter(outputFilePath))
+                    {
+                        writer.WriteLine(header); // 写入列名
+
+                        for (int i = 0; i < linesPerFile && !reader.EndOfStream; i++)
+                        {
+                            writer.WriteLine(reader.ReadLine());
+                            currentLine++;
+                        }
+                    }
+
+                    fileNumber++;
+                }
+            }
+        }
+    }
+}
